7fcfcfe067a5fc22ce3b387ffda2eecfd990186f
Yonghwan SO <sio4@users.sf.net>
2014-11-03 14:40:33 +0900

    add custom application logger

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 require File.expand_path('../config', __FILE__)
+require 'app_logger'
 
 # Initialize the Rails application.
 Rails.application.initialize!
